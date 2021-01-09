c6e4468d5c46c0a67b301e0e6a687cf62df02083
fongvang09 <76004558+fongvang09@users.noreply.github.com>
2021-01-08 23:20:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -51,17 +51,6 @@ startBtn.addEventListener("click", function (e) {
     for (let i = 0; i < wordArr.length; i++) {
         wordArr[i] = "_"
     };
-    console.log(wordArr);
-
-
-
-    // var imageArr = chosenImg.split("");
-    // for (let i = 0; i < imageArr.length; i++) {
-    //     __Arr.push("_");
-    // };
-    // __Arr = __Arr.join(" ");
-
-    // wordDisplay.textContent = __Arr;
 })
 
 if (typeof(Storage) !== "undefined") {
@@ -70,19 +59,3 @@ if (typeof(Storage) !== "undefined") {
 } else {
     document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
-
-// var images = [],
-// index = 0;
-// images[0] = "<img src='https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/LeafEmpty.png' alt='Visit Computer Hope'></a>";
-// images[1] = "<img src='https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/LeafFull.png' alt='Computer History'></a>";
-// images[2] = "<img src='https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/FlowerEmpty.png' alt='Visit Computer Hope'></a>";
-// images[3] = "<img src='https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/FlowerFull.png' alt='Visit Computer Hope'></a>";
-// images[4] = "<img src='https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/CircledFlowerEmpty.png' alt='Visit Computer Hope'></a>";
-// images[5] = "<img src='https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/CircledFlowerFull.png' alt='Visit Computer Hope'></a>";
-// images[6] = "<img src='https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/CircledLeafEmpty.png' alt='Visit Computer Hope'></a>";
-// images[7] = "<img src='https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/CircledLeafFull.png' alt='Visit Computer Hope'></a>";
-
-
-var imageList = ["https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/LeafEmpty.png", "https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/LeafFull.png", "https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/FlowerEmpty.png", 
-"https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/FlowerFull.png", "https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/CircledFlowerEmpty.png", "https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/CircledFlowerFull.png",
-"https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/CircledLeafEmpty.png", "https://assets.mythictrap.com/mistsOfTirnaScithe/miniGame/CircledLeafFull.png"];
